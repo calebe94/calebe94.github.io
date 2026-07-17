373dc6d4da41af9427261a75f29ebd817faaa09f
calebe94 <calebe94@disroot.org>
2026-07-17 17:09:49 -0300

    feat(dns): add CNAME emitter to Quartz build

--- quartz.config.ts
+++ quartz.config.ts
@@ -87,6 +87,7 @@ const config: QuartzConfig = {
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.Favicon(),
+      Plugin.CNAME()
       // Comment out CustomOgImages to speed up build time
       // Plugin.CustomOgImages(),
     ],
